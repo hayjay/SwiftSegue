117cbffa509362588b96436f67f5b10436938951
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-10-27 10:24:06 +0100

    passed data from ViewController to SecondViewController

--- ExampleSegues/SecondViewController.swift
+++ ExampleSegues/SecondViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class SecondViewController: UIViewController {
     var textPassedOver : String? //optional string
+    
     @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //passing the data coming from the previous page after the button is been clicked
+        //we are able to do this because once the previous segue that link to this screen is been pressed, weve captured the data we needed from there
+        label.text = textPassedOver
         // Do any additional setup after loading the view.
     }
     
