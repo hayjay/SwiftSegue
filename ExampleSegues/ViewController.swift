fbd5b94f2c56152385e17a5df598f59828f0e96b
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-10-27 09:40:35 +0100

    Initial Commit

---
+++ ExampleSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSegues
+//
+//  Created by Nurudeen Ajayi on 27/10/2019.
+//  Copyright © 2019 Nurudeen Ajayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
