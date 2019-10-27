c2faa4c0faa74a51eab3a5cfb62150c16f82c486
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-10-27 10:13:58 +0100

    segues with storyboards

---
+++ ExampleSegues/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  ExampleSegues
+//
+//  Created by Nurudeen Ajayi on 27/10/2019.
+//  Copyright © 2019 Nurudeen Ajayi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    var textPassedOver : String? //optional string
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ExampleSegues/ViewController.swift
+++ ExampleSegues/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+//    PERFORM SEGUE WHEN THIS BUTTON IS PRESSED
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    //this is a builtin swift method that gets executed before every segue gets triggered
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            //when a user clicks the button from the first screen
+            //while typing segue.destination the intellisence tells us that segue.destination is of type UIViewController
+//            which means that we could type cast or overite the UIViewController as the controller we want (SecondViewController)
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.textPassedOver = textField.text
+        }
+    }
 }
 
